a575d2a0470af482e19b43d52c1c371648e9dda6
Derek Millar <derek.millartech@gmail.com>
2020-03-28 13:33:31 +0000

    Add all component files

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export class Track extends React.Component {
+    renderAction() {
+        if(isRemoval) {
+            return '-'
+        } else {
+            return '+'
+        }
+    }
+    
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3><!-- track name will go here --></h3>
+                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                </div>
+                <button className="Track-action">{this.renderAction}</button>
+            </div>
+                    )
+    }
+}
